e920bcbb6934c228cedfbe2e0c6dedd726d6c25e
AgathePons <ponsagathe37@gmail.com>
2022-11-09 14:48:13 +0100

    add: play with functional method on lists

--- src/test/java/poe/model/play/PlayCollectionString.java
+++ src/test/java/poe/model/play/PlayCollectionString.java
@@ -32,8 +32,38 @@ public class PlayCollectionString {
     @Test
     void playWithList3() {
         // since Java 11
-        List<String> animals = List.of("Cat", "Dog", "Bird", "Bunny");
+        List<String> animals = List.of("Cat", "Dog", "Bird", "Bunny", "Unicorn", "snake");
         System.out.println(animals);
-        System.out.println(animals.getClass());
+        System.out.println(animals.getClass()); // this List is immutable, we cannot use the add() method for example
+        // loops: statements
+        for (String animal: animals) {
+            System.out.println("\t- I pet the " + animal.toUpperCase() + " <3");
+        }
+        System.out.println("And................");
+        for (int i = 0; i < animals.size(); i++) {
+            System.out.println("\t-> I feed the " + animals.get(i));
+        }
+        System.out.println("And................");
+        // loops: functional
+        // .forEach()
+        animals.forEach(animal -> System.out.println("\t>> Who let the " + animal + " out? Woo! Woowoo!"));
+        System.out.println("And................");
+        // .reduce() .map() .filter()
+        List<Integer> countLetters = animals.stream()
+                .map(animal -> animal.length())
+                .toList(); // toList() java 17
+        System.out.println(countLetters);
+        System.out.println("And................");
+        // other pipeline
+        List<Integer> countLetters2 = animals.stream()
+                .filter(animal -> animal.endsWith("e"))
+                .map(String::length) // function reference
+                .toList();
+        System.out.println(countLetters2);
+        // other pipeline with reduce()
+        int totalCountLetters = animals.stream()
+                .mapToInt(String::length)// prevent the autoboxing in Integer and allow to use int methods
+                .sum();
+        System.out.println("There are " + totalCountLetters + " letters");
     }
 }
